da8dba26b1642fe8f53f5c2a29c26619054063aa
tangzl7 <3155837867@qq.com>
2021-09-05 13:40:35 +0800

    add sdk single img infer

---
+++ infer/sdk/main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+Copyright 2020 Huawei Technologies Co., Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+# import StreamManagerApi.py
+import sys
+
+from StreamManagerApi import *
+
+
+if __name__ == '__main__':
+    # init stream manager
+    streamManagerApi = StreamManagerApi()
+    ret = streamManagerApi.InitManager()
+    if ret != 0:
+        print("Failed to init Stream manager, ret=%s" % str(ret))
+        exit()
+
+    # create streams by pipeline config file
+    with open("../data/face_recognition_for_tracking.pipeline", 'rb') as f:
+        pipelineStr = f.read()
+    ret = streamManagerApi.CreateMultipleStreams(pipelineStr)
+    if ret != 0:
+        print("Failed to create Stream, ret=%s" % str(ret))
+        exit()
+
+    # Construct the input of the stream
+    dataInput = MxDataInput()
+    file_path = sys.argv[1]
+    with open(file_path, 'rb') as f:
+        dataInput.data = f.read()
+
+    # Inputs data to a specified stream based on streamName.
+    streamName = b'im_facerecognitionfortracking'
+    inPluginId = 0
+    uniqueId = streamManagerApi.SendDataWithUniqueId(streamName, inPluginId, dataInput)
+    if uniqueId < 0:
+        print("Failed to send data to stream.")
+        exit()
+
+    # Obtain the inference result by specifying streamName and uniqueId.
+    inferResult = streamManagerApi.GetResultWithUniqueId(streamName, uniqueId, 3000)
+    if inferResult.errorCode != 0:
+        print("GetResultWithUniqueId error. errorCode=%d, errorMsg=%s" % (
+            inferResult.errorCode, inferResult.data.decode()))
+        exit()
+
+    # print the infer result
+    print(inferResult.data.decode())
+
+    # destroy streams
+    streamManagerApi.DestroyAllStreams()
